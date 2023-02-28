45d3fbbc4040d76e1e7128da91e181da6d2b728b
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2023-02-28 12:46:51 -0600

    can add users to the database

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 
 const mongoose = require("mongoose");
 require('dotenv').config();
-//for server
-const express = require("express");
-//for secruity cross origin
-const cors = require("cors");
-//entire authentication library
-const passport = require("passport");
-//using this as a strategy
-const passportLocal = require("passport-local").Strategy;
-//to parse all of the cookies we use for authentication
-const cookieParser = require("cookie-parser");
-//for hashing that passwords
-const bcrypt = require("bcryptjs");
+const express = require("express"); //for server
+const cors = require("cors"); //for secruity cross origin
+const passport = require("passport"); //entire authentication library
+const passportLocal = require("passport-local").Strategy; //using this as a strategy
+const cookieParser = require("cookie-parser"); //to parse all of the cookies we use for authentication
+const bcrypt = require("bcryptjs"); //for hashing that passwords
 const session = require("express-session");
-//parse the request and response objects
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); //parse the request and response objects
+
 //environmental variables
 const app = express();
 const mongoURI = process.env.MONGO_URI
@@ -34,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
     cors({
         //nothing will work if this isnt there
-        //origin should be the location of the react app we are connecting it to
+        //origin should be the location of the react app we are connecting it to but you can white list it using *
         origin: "http://localhost:3000",
         credentials: true,
     })
@@ -57,7 +51,22 @@ app.post('/login', (req,res) => {
     console.log(req.body)
 })
 app.post('/register', (req,res) => {
-    console.log(req.body)
+    //test to see if the username already exists
+    User.findOne({ username: req.body.username })
+    //need to use then because some mongoose functions no longer accept callback functions
+    .then(async (err,doc) => {
+        if(err) throw err;
+        //a document means that there is already someone registered in the database with the same document
+        if(doc) res.send('User Already Exists');
+        if(!doc){
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.password
+            });
+            await newUser.save();
+            res.send('User Created');
+        }
+    })
 })
 app.post('/login', (req,res) => {
     console.log(req.body)
